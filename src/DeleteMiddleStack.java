2e24e4659a7b593c322dac386ee74a5b40005f8b
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-06 21:46:50 -0700

    initialized a method so that i can perform recursive function easily

--- src/DeleteMiddleStack.java
+++ src/DeleteMiddleStack.java
@@ -1,5 +1,13 @@
+import java.util.Stack;
+
 public class DeleteMiddleStack {
-    
+
+
+
+    public static void deleteMiddleStackElement(int i, Stack<Integer> st, int n){
+
+    }
+
     public static void main(String[] args) {
 
     }
