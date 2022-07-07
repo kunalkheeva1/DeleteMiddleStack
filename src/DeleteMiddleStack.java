236c07a516e340a4fcf88cacf2622518a02a025a
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-06 22:10:12 -0700

    added comments for better understanding

--- src/DeleteMiddleStack.java
+++ src/DeleteMiddleStack.java
@@ -3,17 +3,23 @@ import java.util.Stack;
 public class DeleteMiddleStack {
 
 public static void deleteMiddleElementRec(int i, Stack<Integer>st, int n){
+
+    // if index becomes n/2 at any time then just delete the element
     if(i==n/2){
         st.pop();
         return;
     }
-    int storage = st.pop();
 
+    //until then store all the elements before n/2 int storage variable
+    int storage = st.pop();
+    // then for every call choose next i
     deleteMiddleElementRec(i+1, st, n);
+
+    // after doing that just push the elements stored in storage
     st.push(storage);
 }
 
-    public static void deleteMiddleStackElement(int i, Stack<Integer> st, int n){
+    public static void deleteMiddleStackElement(Stack<Integer> st, int n){
     deleteMiddleElementRec(0,st,n);
 
     }
