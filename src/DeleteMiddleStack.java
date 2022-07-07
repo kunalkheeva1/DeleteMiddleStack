cc59e2f4eafa458e955e770619585153c590b0db
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-06 22:05:55 -0700

    created amehtod to delete an element from the mid of the stack

--- src/DeleteMiddleStack.java
+++ src/DeleteMiddleStack.java
@@ -2,7 +2,16 @@ import java.util.Stack;
 
 public class DeleteMiddleStack {
 
+public static void deleteMiddleElementRec(int i, Stack<Integer>st, int n){
+    if(i==n/2){
+        st.pop();
+        return;
+    }
+    int storage = st.pop();
 
+    deleteMiddleElementRec(i+1, st, n);
+    st.push(storage);
+}
 
     public static void deleteMiddleStackElement(int i, Stack<Integer> st, int n){
 
